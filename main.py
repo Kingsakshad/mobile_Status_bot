8b5c44a2069b53a1914091008e2e090c3b690c20
Known As X <mrxjoker696@gmail.com>
2022-03-14 18:43:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import ast
 import inspect
+from lib2to3.pgen2 import token
 import re
+from click import command
 import discord
+from discord.ext import commands
 
+token = "Your Bot Token"
+bot = commands.Bot(command_prefix='.')
 
 def source(o):
     s = inspect.getsource(o).split("\n")
@@ -21,3 +26,10 @@ loc = {}
 exec(compile(ast.parse(patched), "<string>", "exec"), discord.gateway.__dict__, loc)
 
 discord.gateway.DiscordWebSocket.identify = loc["identify"]
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you"))
+    print('Ready')
+
+bot.run(token)
